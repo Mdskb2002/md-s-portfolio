808f6797f6bd5ddad111f3ddba85bfc8a2be7d46
Mohammad Sakib Kadarbhai <102642026+Mdskb2002@users.noreply.github.com>
2024-05-28 22:30:38 +0530

    Add files via upload

---
+++ md-s-portfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Mohammad Sakib</span> Full Stack Developer aspirant skilled at working in both front-end and back-end development workflows. To give my best as an individual, to be a good team player and to focus on the organization growth by gainfully utilizing my knowledge, skills and abilities."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
